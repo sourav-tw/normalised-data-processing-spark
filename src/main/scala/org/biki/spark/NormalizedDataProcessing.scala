e505dd11ffabec0af019c7f64bc08fa60572b934
Sourav Bhattacharjee <sourav.b@thoughtworks.com>
2022-11-17 15:02:57 +0530

    [Sourav] Add. Handle upsert

--- src/main/scala/org/biki/spark/NormalizedDataProcessing.scala
+++ src/main/scala/org/biki/spark/NormalizedDataProcessing.scala
@@ -3,9 +3,9 @@ package org.biki.spark
 import org.apache.log4j.Logger
 import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
-import java.sql.{DriverManager, PreparedStatement, ResultSet}
+import java.sql.{Connection, DriverManager, PreparedStatement, ResultSet}
 import java.util.{Properties, UUID}
 
 object NormalizedDataProcessing {
@@ -15,27 +15,9 @@ object NormalizedDataProcessing {
   def main(args: Array[String]): Unit = {
 
     logger.info("Creating connection properties for connecting to postgresql")
-    val jdbcUrl = scala.util.Properties.envOrElse("JDBC_URL","jdbc:postgresql://localhost:5432/bookshop-db")
-    val jdbcDriver = scala.util.Properties.envOrElse("JDBC_DRIVER","org.postgresql.postgresql")
-    val user = scala.util.Properties.envOrElse("DB_USERNAME","postgres")
-    val password = scala.util.Properties.envOrElse("DB_PASSWORD","postgres")
+    val connectionProperties: Properties = prepareDBConnectionProperties
 
-    val connectionProperties = new Properties
-    connectionProperties.put("jdbcUrl", jdbcUrl)
-    connectionProperties.put("jdbcDriver", jdbcDriver)
-    connectionProperties.put("user", user)
-    connectionProperties.put("password", password)
-
-    val schema = StructType(List(
-      StructField("firstName", StringType),
-      StructField("lastName", StringType),
-      StructField("id", LongType),
-      StructField("address", StructType(Seq(
-        StructField("city", StringType),
-        StructField("district", StringType),
-        StructField("pinCode", StringType)
-      ))),
-    ))
+    val schema: StructType = InferJSONSchema
 
     logger.info("Create Spark Session")
     val spark = SparkSession.builder()
@@ -43,18 +25,9 @@ object NormalizedDataProcessing {
       .master("local[3]")
       .getOrCreate()
 
-    logger.info("Read JSON data")
-    val employeeDF = spark.read
-      .format("json")
-      .schema(schema)
-      .option("path", "data/")
-      .load()
+    val employeeDF: DataFrame = readEmployeeDataRaw(schema, spark)
 
-    logger.info("Denormalize complex JSON data")
-    val denormalizedEmployeeDF = employeeDF.withColumn("city", expr("address.city"))
-      .withColumn("district", expr("address.district"))
-      .withColumn("pinCode", expr("address.pinCode"))
-      .drop("address")
+    val denormalizedEmployeeDF: DataFrame = denormalizeEmployeeData(employeeDF)
 
     logger.info("Get Spark Context")
     val context = spark.sparkContext
@@ -74,7 +47,7 @@ object NormalizedDataProcessing {
 
       //Class.forName(jdbcDriver)
 
-      val dbConnection = DriverManager.getConnection(jdbcUrl, user, password)
+      val dbConnection: Connection = DriverManager.getConnection(jdbcUrl, user, password)
       dbConnection.setAutoCommit(false)
 
       val db_batchSize = 50
@@ -83,52 +56,15 @@ object NormalizedDataProcessing {
       logger.info("Loop through each data row in the partition")
       partition.grouped(db_batchSize).foreach(batch => {
         batch.foreach { row => {
-          logger.info("Read column values from individual row")
-          val employeeIdIdx = row.fieldIndex("id")
-          val employeeId = row.getLong(employeeIdIdx)
+          val (employeeId: Long, firstName: String, lastName: String, city: String, district: String, pinCode: String) = getDataElements(row)
 
-          val firstNameIdx = row.fieldIndex("firstName")
-          val firstName = row.getString(firstNameIdx)
-
-          val lastNameIdx = row.fieldIndex("lastName")
-          val lastName = row.getString(lastNameIdx)
-
-          val cityIdx = row.fieldIndex("city")
-          val city = row.getString(cityIdx)
-
-          val districtIdx = row.fieldIndex("district")
-          val district = row.getString(districtIdx)
-
-          val pinCodeIdx = row.fieldIndex("pinCode")
-          val pinCode = row.getString(pinCodeIdx)
+          var (addressStmt: PreparedStatement, uniqueAddressId: String) = getUniqueAddress(dbConnection,city,district,pinCode)
+          if(uniqueAddressId == "")
+            uniqueAddressId = null
 
           logger.info("Check if employee data exists")
-          val sqlString = "SELECT * FROM public.employee_details_view WHERE employeeId=?"
-          var stmt: PreparedStatement = dbConnection.prepareStatement(sqlString)
 
-          stmt.setLong(1, employeeId)
-          val result = stmt.executeQuery()
-          var count: Int = 0
-
-          while (result.next()) {
-            count = 1
-          }
-
-          logger.info("Check for existing address")
-          val addressSQLString = "SELECT * FROM public.employee_details_view WHERE city=? AND district=? AND pinCode=?"
-          var addressStmt: PreparedStatement = dbConnection.prepareStatement(addressSQLString, ResultSet.TYPE_SCROLL_SENSITIVE,
-            ResultSet.CONCUR_UPDATABLE)
-
-          addressStmt.setString(1, city)
-          addressStmt.setString(2, district)
-          addressStmt.setString(3, pinCode)
-
-          val addressResult = addressStmt.executeQuery()
-          var uniqueAddressId: String = null
-
-          if (addressResult.first())
-            uniqueAddressId = addressResult.getString("ain")
-
+          var (stmt: PreparedStatement, count: Int) = checkForExistingEmployee(dbConnection,employeeId)
           logger.info("Determine whether the current record is needed to be updated or inserted")
           var dmlOption: String = null
           if (count > 0) {
@@ -139,56 +75,38 @@ object NormalizedDataProcessing {
           }
 
           if (dmlOption == "U") {
-
+            var updateSQLString: String = null
             if (uniqueAddressId == null){
-              val updateSQLString =
-                """
-                  |WITH INSERTED as (
-                  |	INSERT INTO public.employeeaddresses(city,district,pinCode) VALUES (?,?,?)
-                  |	RETURNING ain
-                  |)
-                  |UPDATE public.employees
-                  |SET ain = (SELECT ain FROM INSERTED)
-                  |WHERE employeeId=?
-                  |""".stripMargin
+               updateSQLString = prepareUpdateStatementWithNewAddress(employeeId, city, district, pinCode)
+            }
+            else {
+              updateSQLString = prepareUpdateStatementWithExistingAddress(employeeId, firstName, lastName, uniqueAddressId)
+            }
 
-              prepareStatement = dbConnection.prepareStatement(updateSQLString)
+            prepareStatement = dbConnection.prepareStatement(updateSQLString)
 
+            if (uniqueAddressId == null) {
               prepareStatement.setString(1, city)
               prepareStatement.setString(2, district)
               prepareStatement.setString(3, pinCode)
               prepareStatement.setLong(4, employeeId)
             }
             else {
-              val updateSQLString = "UPDATE public.employees SET firstName=?,lastName=?,ain=? WHERE employeeId=?"
-              prepareStatement = dbConnection.prepareStatement(updateSQLString)
-
               prepareStatement.setString(1, firstName)
               prepareStatement.setString(2, lastName)
-              prepareStatement.setObject(3,UUID.fromString(uniqueAddressId))
+              prepareStatement.setObject(3, UUID.fromString(uniqueAddressId))
               prepareStatement.setLong(4, employeeId)
             }
           }
+
           else if (dmlOption == "I") {
             var insertSQLString:String = null
+
             if (uniqueAddressId == null) {
-              insertSQLString =
-              """
-                |WITH INSERTED as (
-                |	INSERT INTO public.employeeaddresses(city,district,pinCode) VALUES (?,?,?)
-                |	RETURNING ain
-                |)
-                |INSERT INTO public.employees(firstname,lastname,employeeId,ain)
-                |SELECT ?,?,?,ain
-                |FROM INSERTED
-                |""".stripMargin
+              insertSQLString = prepareInsertStatmentWithNewAddress()
             }
             else {
-              insertSQLString =
-                """
-                  |INSERT INTO public.employees(firstname,lastname,employeeId,ain)
-                  |VALUES (?,?,?,?)
-                  |""".stripMargin
+              insertSQLString = prepareInsertStatementWithExistingAddress()
             }
 
             prepareStatement = dbConnection.prepareStatement(insertSQLString)
@@ -226,4 +144,140 @@ object NormalizedDataProcessing {
       logger.info("Stop Spark Session")
       spark.stop()
   }
+
+  private def getDataElements(row: Row) = {
+    logger.info("Read column values from individual row")
+    val employeeIdIdx = row.fieldIndex("id")
+    val employeeId = row.getLong(employeeIdIdx)
+
+    val firstNameIdx = row.fieldIndex("firstName")
+    val firstName = row.getString(firstNameIdx)
+
+    val lastNameIdx = row.fieldIndex("lastName")
+    val lastName = row.getString(lastNameIdx)
+
+    val cityIdx = row.fieldIndex("city")
+    val city = row.getString(cityIdx)
+
+    val districtIdx = row.fieldIndex("district")
+    val district = row.getString(districtIdx)
+
+    val pinCodeIdx = row.fieldIndex("pinCode")
+    val pinCode = row.getString(pinCodeIdx)
+    (employeeId, firstName, lastName, city, district, pinCode)
+  }
+
+  private def denormalizeEmployeeData(employeeDF: DataFrame) = {
+    logger.info("Denormalize complex JSON data")
+    val denormalizedEmployeeDF = employeeDF.withColumn("city", expr("address.city"))
+      .withColumn("district", expr("address.district"))
+      .withColumn("pinCode", expr("address.pinCode"))
+      .drop("address")
+    denormalizedEmployeeDF
+  }
+
+  private def readEmployeeDataRaw(schema: StructType, spark: SparkSession) = {
+    logger.info("Read JSON data")
+    val employeeDF = spark.read
+      .format("json")
+      .schema(schema)
+      .option("path", "data/")
+      .load()
+    employeeDF
+  }
+
+  private def InferJSONSchema = {
+    val schema = StructType(List(
+      StructField("firstName", StringType),
+      StructField("lastName", StringType),
+      StructField("id", LongType),
+      StructField("address", StructType(Seq(
+        StructField("city", StringType),
+        StructField("district", StringType),
+        StructField("pinCode", StringType)
+      ))),
+    ))
+    schema
+  }
+
+  private def prepareDBConnectionProperties = {
+    val jdbcUrl = scala.util.Properties.envOrElse("JDBC_URL", "jdbc:postgresql://localhost:5432/bookshop-db")
+    val jdbcDriver = scala.util.Properties.envOrElse("JDBC_DRIVER", "org.postgresql.postgresql")
+    val user = scala.util.Properties.envOrElse("DB_USERNAME", "postgres")
+    val password = scala.util.Properties.envOrElse("DB_PASSWORD", "postgres")
+
+    val connectionProperties = new Properties
+    connectionProperties.put("jdbcUrl", jdbcUrl)
+    connectionProperties.put("jdbcDriver", jdbcDriver)
+    connectionProperties.put("user", user)
+    connectionProperties.put("password", password)
+    connectionProperties
+  }
+
+  private def checkForExistingEmployee(dbConnection:Connection,employeeId:Long) = {
+    val sqlString = "SELECT * FROM public.employee_details_view WHERE employeeId=?"
+    var stmt: PreparedStatement = dbConnection.prepareStatement(sqlString)
+
+    stmt.setLong(1, employeeId)
+    val result = stmt.executeQuery()
+    var count: Int = 0
+
+    while (result.next()) {
+      count = 1
+    }
+    (stmt, count)
+  }
+  private def getUniqueAddress(dbConnection:Connection,city:String,district:String,pinCode:String) = {
+    logger.info("Check for existing address")
+    val addressSQLString = "SELECT * FROM public.employee_details_view WHERE city=? AND district=? AND pinCode=?"
+    var addressStmt: PreparedStatement = dbConnection.prepareStatement(addressSQLString, ResultSet.TYPE_SCROLL_SENSITIVE,
+      ResultSet.CONCUR_UPDATABLE)
+
+    addressStmt.setString(1, city)
+    addressStmt.setString(2, district)
+    addressStmt.setString(3, pinCode)
+
+    val addressResult = addressStmt.executeQuery()
+    var uniqueAddressId: String = ""
+
+    if (addressResult.first())
+      uniqueAddressId = addressResult.getString("ain")
+    (addressStmt, uniqueAddressId)
+  }
+
+  private def prepareInsertStatmentWithNewAddress(): String = {
+    """
+      |WITH INSERTED as (
+      |	INSERT INTO public.employeeaddresses(city,district,pinCode) VALUES (?,?,?)
+      |	RETURNING ain
+      |)
+      |INSERT INTO public.employees(firstname,lastname,employeeId,ain)
+      |SELECT ?,?,?,ain
+      |FROM INSERTED
+      |""".stripMargin
+  }
+
+  private def prepareInsertStatementWithExistingAddress(): String = {
+    """
+      |INSERT INTO public.employees(firstname,lastname,employeeId,ain)
+      |VALUES (?,?,?,?)
+      |""".stripMargin
+  }
+
+  private def prepareUpdateStatementWithNewAddress(employeeId: Long, city: String, district: String, pinCode: String): String = {
+    """
+      |WITH INSERTED as (
+      |	INSERT INTO public.employeeaddresses(city,district,pinCode) VALUES (?,?,?)
+      |	RETURNING ain
+      |)
+      |UPDATE public.employees
+      |SET ain = (SELECT ain FROM INSERTED)
+      |WHERE employeeId=?
+      |""".stripMargin
+  }
+
+  private def prepareUpdateStatementWithExistingAddress(employeeId: Long, firstName: String, lastName: String, uniqueAddressId: String): String = {
+    "UPDATE public.employees SET firstName=?,lastName=?,ain=? WHERE employeeId=?"
+  }
+
 }
